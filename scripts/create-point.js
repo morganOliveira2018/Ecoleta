26a40039f87098f2ea36646dc494d548077ed87d
Morgana Ribeiro <morganfrime2017@gmail.com>
2020-06-04 21:02:27 -0300

    Aula 02 - Finalizada

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -13,27 +13,29 @@ function populateUFs () {
 populateUFs()
 
 function getCities(event) {
-    // const citySelect = document.querySelector("select[name=city]")
-    // const stateinput = document.querySelector("select[name=state]")
-    const citySelect = document.querySelector("select[name=city]")
-
+    const citySelect = document.querySelector("[name=city]")
+    const stateInput = document.querySelector("[name=state]")
     const ufValue = event.target.value
+    // const citySelect = document.querySelector("select[name=city]")
+    // const stateinput = document.querySelector("input[name=state]")
+    
 
-    //const indexOfSelectedState = event.target.selectedIndex
-    //stateInput.value = event.target.options[indexOfSelectedState].text
+    const indexOfSelectedState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectedState].text
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
     // Nessas duas linhas de código, sera feita a limpeza do campo, caso seja escolhido outro estado, dessa forma, nao ficaram cidades acumuladas.
-    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
-    citySelect.disabled = true
+    // citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    //citySelect.disabled = true
 
     fetch(url)
-    .then( res => res.json)
+    .then( res => res.json())
     .then( cities => {
         for( const city of cities ) {
+             /* Apresenta a opção selecionada no Front do HTML */
             citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
-            
+           
         }
         citySelect.disabled = false
     })
@@ -42,4 +44,4 @@ function getCities(event) {
 document
         .querySelector("select[name=uf]")
         .addEventListener("change", getCities)
-        /* se quiser pode ser: getCities(event) */
+        /* Se quiser pode ser também: getCities(event) */
